e77cbfe10a0e10afd235c583769200b999d86e60
Nada <nadakhaled658@yahoo.com>
2024-04-24 00:14:54 +0200

    the user data has been done

--- src/components/The_Register.vue
+++ src/components/The_Register.vue
@@ -113,7 +113,9 @@
             >{{ activity }}</v-chip
           >
         </v-chip-group>
-        <p class="mb-4 text-right">{{ Charities.Charities_specialty }}</p>
+        <p class="mb-4 text-right">
+          {{ Charities.Charities_specialty }}
+        </p>
         <p class="mb-2 text-right">أنواع الباقات</p>
         <!--the Charities can select multiple items from the group and it will be displayed in a paragraph-->
         <v-chip-group
@@ -513,9 +515,5 @@ export default {
 <style lang="scss" scoped>
 form {
   width: 95% !important;
-}
-.chip {
-  display: flex !important;
-  flex-direction: column !important;
 }
 </style>
