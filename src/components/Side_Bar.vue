71153be1735bfacfb827e64a601e175129cea3ef
Nada <nadakhaled658@yahoo.com>
2024-05-02 17:31:36 +0300

    edited the side bar

--- src/components/Side_Bar.vue
+++ src/components/Side_Bar.vue
@@ -1,34 +1,22 @@
 <template>
     <!-- navigation-drawer  -->
     <v-layout>
-        <v-navigation-drawer permanent rail location="right">
+        <v-navigation-drawer rail permanent location="right">
             <v-list nav class="pt-16 pa-5 px-2">
                 <v-list-item
                     link
                     @click="$router.push('/')"
                     prepend-icon="mdi-home-outline"
-                    >الصفحة الرئيسية
+                >
                 </v-list-item>
 
                 <v-list-item link prepend-icon="mdi-chart-pie-outline">
-                    التقارير</v-list-item
-                >
-                <v-list-item link prepend-icon="mdi-cog-outline">
-                    الاعدادات</v-list-item
-                >
-
-                <!-- <br /> -->
-                <!-- <br /> -->
-                <!-- <br /> -->
-                <!-- <v-list-item
-                        class="text-red lighten-2"
-                        link
-                        @click="Sign_Out"
-                        prepend-icon="mdi-export"
-                    >
-                        تسجيل خروج
-                    </v-list-item> -->
+                </v-list-item>
+                <v-list-item link prepend-icon="mdi-cog-outline"> </v-list-item>
             </v-list>
         </v-navigation-drawer>
     </v-layout>
 </template>
+<script scoped>
+export default {};
+</script>
