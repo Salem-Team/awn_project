78f20d42daff387ea8f1b4c720b90fa6a1482402
Nada <nadakhaled658@yahoo.com>
2024-05-05 23:36:11 +0300

    added the cases

--- src/views/DashBoard_Admin.vue
+++ src/views/DashBoard_Admin.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="Dash_board">
-        <!-- navigation-drawer  -->
+        <!-- NavBar  -->
         <Side_Bar />
         <v-container fluid>
             <v-row>
@@ -13,69 +13,7 @@
                             {{ tab }}
                         </h1>
                         <!-- Info Status -->
-                        <div class="d-flex ga-3 mb-3">
-                            <v-menu>
-                                <template v-slot:activator="{ props }">
-                                    <v-btn color="primary" v-bind="props">
-                                        ترتيب حسب
-                                        <v-icon>mdi-filter-variant</v-icon>
-                                    </v-btn>
-                                </template>
-                                <v-list>
-                                    <v-list-item @click="funLatestClicked">
-                                        <v-list-item-title>
-                                            الاحدث للاقدم
-                                        </v-list-item-title>
-                                    </v-list-item>
-                                    <v-list-item @click="funAtZClicked">
-                                        <v-list-item-title>
-                                            أبجدى تصاعدى
-                                        </v-list-item-title>
-                                    </v-list-item>
-                                    <v-list-item @click="funZtAClicked">
-                                        <v-list-item-title>
-                                            أبجدى تنازلى</v-list-item-title
-                                        >
-                                    </v-list-item>
-                                    <v-list-item @click="funCards_STL">
-                                        <v-list-item-title
-                                            >تصاعدى حسب العجز
-                                        </v-list-item-title>
-                                    </v-list-item>
-                                    <v-list-item @click="funCards_LTS">
-                                        <v-list-item-title>
-                                            ننازلى حسب العجز
-                                        </v-list-item-title>
-                                    </v-list-item>
-                                    <v-list-item @click="funFat_STL">
-                                        <v-list-item-title>
-                                            تصاعدى حسب الدخل
-                                        </v-list-item-title>
-                                    </v-list-item>
-                                    <v-list-item @click="funFat_LTS">
-                                        <v-list-item-title>
-                                            تنازلى حسب الدخل
-                                        </v-list-item-title>
-                                    </v-list-item>
-                                    <v-list-item @click="funCalories_STL">
-                                        <v-list-item-title>
-                                            تصاعدى حسب المطلوب
-                                        </v-list-item-title>
-                                    </v-list-item>
-                                    <v-list-item @click="funCalories_LTS">
-                                        <v-list-item-title>
-                                            تنازلى حسب المطلوب
-                                        </v-list-item-title>
-                                    </v-list-item>
-                                </v-list>
-                            </v-menu>
-                            <v-btn>
-                                <v-icon
-                                    style="font-size: 20px !important"
-                                    @click="dialog = true"
-                                    >mdi-plus</v-icon
-                                >
-                            </v-btn>
+                        <div v-if="tab === 'المحتاجين'">
                             <v-btn @click="Swap">
                                 <v-icon>mdi-view-grid-outline</v-icon>
                             </v-btn>
@@ -83,29 +21,6 @@
                     </div>
                 </v-col>
             </v-row>
-        </v-container>
-        <v-container>
-            <v-dialog v-model="dialog" width="90%">
-                <div class="popup bg-white w-100 rounded">
-                    <div class="header">
-                        <div>إضافة الحالات</div>
-                        <font-awesome-icon
-                            :icon="['fas', 'xmark']"
-                            @click="dialog = false"
-                        />
-                    </div>
-                    <div class="body">
-                        <div class="box">
-                            <font-awesome-icon :icon="['fas', 'keyboard']" />
-                            <div>يدوي</div>
-                        </div>
-                        <div class="box">
-                            <font-awesome-icon :icon="['fas', 'file-excel']" />
-                            <div>إكسل</div>
-                        </div>
-                    </div>
-                </div>
-            </v-dialog>
         </v-container>
         <v-container fluid>
             <v-row>
@@ -119,7 +34,8 @@
                         </v-card>
                         <v-window v-model="tab">
                             <v-window-item value="المحتاجين">
-                                <DashboardCharitys /> <StatusInformation
+                                <DashboardCharitys ref="childComponentRef" />
+                                <StatusInformation
                             /></v-window-item>
 
                             <v-window-item value="الجمعيات">
@@ -134,86 +50,81 @@
 </template>
 
 <script>
+// import Xlsx File
+import readXlsxFile from "read-excel-file";
+// import Components
+import CharityView from "@/components/Charity_View.vue";
 import DashboardCharitys from "@/components/DashboardCharitys.vue";
 import StatusInformation from "@/components/StatusInformation.vue";
-import CharityView from "@/components/Charity_View.vue";
 import Side_Bar from "@/components/Side_Bar.vue";
 
 export default {
     inject: ["Emitter"],
-    name: "HomeView",
+    name: "Dash_board",
     components: {
         DashboardCharitys,
         StatusInformation,
-        CharityView,
         Side_Bar,
+        CharityView,
     },
     data() {
         return {
-            dialog: false,
+            items: [],
+            isActive: false,
+            Cases: [],
             tab: null,
-            User: {
-                User_State: true,
-                User_name: "",
-                User_FullName: "",
-            },
         };
     },
+    mounted() {
+        this.$nextTick(() => {
+            if (this.$refs.childComponentRef) {
+                this.$refs.childComponentRef.Get_data();
+            } else {
+                console.error("Child component reference not found.");
+            }
+        });
+    },
     methods: {
-        // Sign_Out() {
-        //     localStorage.removeItem("id");
-        //     this.User.User_State = true;
-        // },
+        // change view
         Swap() {
-            this.Emitter.emit("swapView");
+            this.Emitter.emit("change_view");
         },
-        // / Firts Function ordered By >>>> Swap BT Latest && Oldest
-        funLatestClicked() {
-            this.Emitter.emit("FunLatest");
-        },
-        // / Seconed  Function ordered By >>>> A To Z
-        funAtZClicked() {
-            this.Emitter.emit("FunATZ");
-        },
-        // / Third  Function ordered By >>>> Z To A
-        funZtAClicked() {
-            this.Emitter.emit("FunZTA");
-        },
-        // / Fourth  Function ordered By Cards >>>> S T L
-        funCards_STL() {
-            this.Emitter.emit("CardsAscending");
-        },
-        funCards_LTS() {
-            this.Emitter.emit("CardsDesaending");
-        },
-        // / Sixth  Function ordered Fat >>>> S T L
-        funFat_STL() {
-            this.Emitter.emit("FatAscending");
-        },
-        // / seventh  Function ordered Fat >>>> L T Z
-        funFat_LTS() {
-            this.Emitter.emit("fatDesaending");
-        },
-        // / Eight  Function ordered calories >>>>S T L
-        funCalories_STL() {
-            this.Emitter.emit("caloriesAscending");
-        },
-        // / nine  Function ordered calories >>>>S T L
-        funCalories_LTS() {
-            this.Emitter.emit("caloriesDesaending");
+        To_Json() {
+            const input = document.getElementById("myinput");
+            readXlsxFile(input.files[0]).then((rows) => {
+                const headers = rows[1];
+                const data = rows.slice(3);
+
+                const objByColumn = {};
+
+                headers.forEach((header, index) => {
+                    const columnData = data.map((row) => row[index]);
+                    const adjacentColumnData = data.map((row) => row[index]);
+
+                    const columnObj = {};
+                    columnData.forEach((value, i) => {
+                        columnObj[value] = adjacentColumnData[i];
+                    });
+
+                    objByColumn[header] = columnObj;
+                });
+
+                console.log(objByColumn);
+            });
         },
     },
 };
 </script>
 <style lang="scss" scoped>
-* {
-    direction: rtl !important;
-}
 .info_stat .info {
     font-size: 20px;
     span {
         font-size: 25px;
     }
+}
+.Dash_board {
+    width: calc(100% - 56px);
+    margin-right: auto;
 }
 .popup {
     padding: 20px;
@@ -254,5 +165,29 @@ export default {
             }
         }
     }
+    // btn style
+}
+label input[type="file"] {
+    display: none;
+}
+label span {
+    cursor: pointer;
+    background: #ddd;
+    border: 2px solid #ddd;
+    border-radius: 4px;
+    padding: 10px 20px;
+    display: flex;
+    align-items: flex-start;
+    align-content: space-around;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+    height: 65px;
+}
+label span:hover {
+    border-color: #09f;
+}
+label span:active {
+    box-shadow: 1px 1px 0 #012;
 }
 </style>
