5ee0b0a7aa2e6646de16bc9403855618b3a5f4a9
Nada <nadakhaled658@yahoo.com>
2024-05-17 00:36:52 +0300

    Update DashboardCharitys.vue

--- src/components/DashboardCharitys.vue
+++ src/components/DashboardCharitys.vue
@@ -1938,7 +1938,7 @@
                 v-if="loading"
             ></v-progress-linear>
         </div>
-        <!-- <div class="text-center">
+        <div class="text-center">
             <v-pagination
                 v-model="currentPage"
                 next-icon="mdi-menu-left"
@@ -1946,7 +1946,7 @@
                 :length="Math.ceil(Cases.length / 5)"
                 :total-visible="5"
             ></v-pagination>
-        </div> -->
+        </div>
     </div>
 </template>
 <script scoped>
